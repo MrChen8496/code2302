d71a34aff5c3feb78c9d4bc3f228a02e12dbfc98
CL <cl@tedu.cn>
2023-05-30 14:34:27 +0800

    2023年5月30日14:34:25 - 通过watch监听phone，进行表单验证

--- day02/demo/vue-project/src/views/Form.vue
+++ day02/demo/vue-project/src/views/Form.vue
@@ -2,7 +2,7 @@
   <div>
     <h1>Form.vue,测试表单与v-model</h1>
     <hr>
-    用户账号：<input v-model="form.name" type="text">
+    用户账号：<input v-model="form.name" type="text"> 
     <hr>
     用户密码：<input v-model="form.pwd" type="password">
     <hr>
@@ -28,8 +28,34 @@
       }
     },
     methods: {
+      /** 点击按钮后执行该方法，提交表单收集的数据 */
       submit() {
         console.log(this.form)
+        // 做表单验证，基于正则表达式  验证账号：[3-10]的单词字符
+        let exp = /^\w{3,10}$/;
+        if(exp.test(this.form.name)){ // 验证通过
+          alert('账号验证通过')
+        }else {
+          alert('账号格式不正确，要求[3-10]单词字符')
+        }
+      }
+    },
+
+    /** watch监听器 */
+    watch: {
+      // 监听form.phone，一旦该属性有变化，则执行该监听方法
+      // 传入的参数：newValue新值, oldValue旧值
+      'form.phone': (newValue, oldValue) => {
+        console.log(`form.phone从${oldValue}变成了${newValue}`)
+        // 验证phone的值是否符合要求：
+        if(newValue.length==11){
+          let exp = /^1[3-9]\d{9}$/;
+          if(exp.test(newValue)){
+            console.log('手机号格式正确')
+          }else {
+            console.error('手机号格式错误')
+          }
+        }
       }
     },
   }
