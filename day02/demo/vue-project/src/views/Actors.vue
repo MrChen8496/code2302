4891e31e06e1ebe151e2c7640d01ae95af121127
CL <cl@tedu.cn>
2023-05-31 14:19:07 +0800

    2023年5月31日14:19:01 - 加载并显示演员列表数据

--- day02/demo/vue-project/src/views/Actors.vue
+++ day02/demo/vue-project/src/views/Actors.vue
@@ -4,6 +4,16 @@
     <h3>演员列表</h3>
     <button @click="listActors">加载演员列表</button>
     <hr>
+
+    <!-- 加载演员列表项 -->
+    <div 
+      class="person" 
+      v-for="item in actors" 
+      :key="item.id">
+      <img :src="item.actor_avatar" alt="">
+      <span>{{item.actor_name}}</span>
+    </div>
+
   </div>
 </template>
 
@@ -11,7 +21,12 @@
   import myaxios from '../http/MyAxios'
 
   export default {
-
+    data() {
+      return {
+        // 保存所有的演员 [{id:1, actor_name:'', actor_avatar:''}]
+        actors: [], 
+      }
+    },
     methods: {
       /** 加载演员列表 */
       listActors() {
@@ -19,6 +34,8 @@
         let params = {page:1, pagesize:100}
         myaxios.get(url, params).then(res=>{
           console.log('演员列表', res)
+          // res.data.data 中保存了演员数组
+          this.actors = res.data.data
         })
       }
     },
@@ -26,5 +43,18 @@
 </script>
 
 <style lang="scss" scoped>
+.person {
+  width: 80px;
+  height: 130px;
+  display: inline-block;
+  margin: 0px 10px 10px 0px;
+  text-align: center;
 
+  img {
+    width: 80px;
+  }
+  span {
+    font-size: 0.9em;
+  }
+}
 </style>
