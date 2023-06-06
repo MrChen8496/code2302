4e38816e8f39b2c61e80ee64c0ae5ee84ceb9ad5
CL <cl@tedu.cn>
2023-06-06 16:00:57 +0800

    2023年6月6日16:00:55 - 搭建演员列表页面的静态页面结构

---
+++ day03/demo/bmdstudios-ms-client/src/components/Person.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="person">
+    <img :src="avatar" alt="">
+    <span>{{name}}</span>
+  </div>
+</template>
+
+<script>
+  export default { 
+    // props选项用于定义当前组件的自定义属性 avatar name
+    // 一旦定义了这些属性，则在使用当前组件时，就可以传参
+    // <person avatar="a.jpg" name='张三' ></person>
+    // <person avatar="b.jpg" name='李四' ></person>
+    // <person avatar="c.jpg" name='王五' ></person>
+    // <person avatar="d.jpg" name='赵六' ></person>
+    // props: ['avatar', 'name']
+    props: {
+      name: {
+        type: String,
+        default: '姓名'
+      },
+      avatar: {
+        type: String,
+        required: true
+      }
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+.person {
+  width: 80px;
+  height: 130px;
+  display: inline-block;
+  margin: 0px 10px 10px 0px;
+  text-align: center;
+
+  img {
+    width: 80px;
+  }
+  span {
+    font-size: 0.9em;
+    display: block;
+    overflow: hidden; 
+    white-space: nowrap;
+    text-overflow: ellipsis;
+  }
+}
+</style>
--- day03/demo/bmdstudios-ms-client/src/views/actor/ActorList.vue
+++ day03/demo/bmdstudios-ms-client/src/views/actor/ActorList.vue
@@ -1,12 +1,35 @@
 <template>
   <div>
     <!-- views/actor/ActorList.vue -->
-    <h1>views/actor/ActorList.vue 列表</h1>
+
+    <!-- 面包屑导航 -->
+    <el-breadcrumb separator="/">
+      <el-breadcrumb-item>演员管理</el-breadcrumb-item>
+      <el-breadcrumb-item>演员列表</el-breadcrumb-item>
+    </el-breadcrumb>
+    <!-- 分隔线 -->
+    <el-divider></el-divider>
+
+    <!-- 表单 -->
+    姓名：
+    <el-input placeholder="姓名关键字" style="width:200px;"></el-input> 
+    &nbsp;
+    <el-button type="primary">查询</el-button>
+    <el-divider content-position="left">列表数据</el-divider>
+
+    <!-- 展示演员列表数据 -->
+    <person name="亮亮" avatar="https://p0.pipi.cn/basicdata/25bfd6d753706dd7c3cf3e4348f18898a9954.jpg?imageView2/1/w/128/h/170"></person>
+
   </div>
 </template>
 
 <script>
-export default {};
+import Person from "@/components/Person.vue"
+
+export default {
+  components: { Person },
+
+};
 </script>
 
 <style lang="scss" scoped>
