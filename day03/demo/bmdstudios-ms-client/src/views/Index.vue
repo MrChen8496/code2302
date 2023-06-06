9a20b8a1e22c61725e2a76f2e700cdf2b0d8ef91
CL <cl@tedu.cn>
2023-06-06 15:34:18 +0800

    2023年6月6日15:34:15 - echarts按需引入示例

--- day03/demo/bmdstudios-ms-client/src/views/Index.vue
+++ day03/demo/bmdstudios-ms-client/src/views/Index.vue
@@ -7,7 +7,39 @@
 </template>
 
 <script>
-import * as echarts from 'echarts';
+// 引入 echarts 核心模块，核心模块提供了 echarts 使用必须要的接口。
+import * as echarts from 'echarts/core';
+// 引入柱状图图表，图表后缀都为 Chart
+import { BarChart,PieChart } from 'echarts/charts';
+// 引入提示框，标题，直角坐标系，数据集，内置数据转换器组件，组件后缀都为 Component
+import {
+  TitleComponent,
+  TooltipComponent,
+  GridComponent,
+  DatasetComponent,
+  TransformComponent,
+  LegendComponent
+} from 'echarts/components';
+// 标签自动布局、全局过渡动画等特性
+import { LabelLayout, UniversalTransition } from 'echarts/features';
+// 引入 Canvas 渲染器，注意引入 CanvasRenderer 或者 SVGRenderer 是必须的一步
+import { CanvasRenderer } from 'echarts/renderers';
+
+// 注册必须的组件
+echarts.use([
+  TitleComponent,
+  TooltipComponent,
+  GridComponent,
+  DatasetComponent,
+  TransformComponent,
+  LegendComponent,
+  BarChart,
+  PieChart,
+  LabelLayout,
+  UniversalTransition,
+  CanvasRenderer
+]);
+
 export default {
   data() {
     return {
@@ -26,13 +58,19 @@ export default {
   
   methods: {
     initChart2(){
+      // 将this.categoryData的数据结构整理成echarts所需要的结构
+      // 现在：{cats:['动作', '悬疑']    counts:[123, 86]}
+      // 目标：[{name:'动作', value:123}, {name:'悬疑', value:86}]
+      let data = []
+      for(let i=0; i<this.categoryData.cats.length; i++){
+        let cat = this.categoryData.cats[i]
+        let count = this.categoryData.counts[i]
+        data.push({name:cat, value:count})
+      }
+      console.log(data)
+
       var myChart = echarts.init(document.getElementById('chart2'));
       let option = {
-        title: {
-          text: 'Referer of a Website',
-          subtext: 'Fake Data',
-          left: 'center'
-        },
         tooltip: {
           trigger: 'item'
         },
@@ -45,13 +83,7 @@ export default {
             name: 'Access From',
             type: 'pie',
             radius: '50%',
-            data: [
-              { value: 1048, name: 'Search Engine' },
-              { value: 735, name: 'Direct' },
-              { value: 580, name: 'Email' },
-              { value: 484, name: 'Union Ads' },
-              { value: 300, name: 'Video Ads' }
-            ],
+            data: data,
             emphasis: {
               itemStyle: {
                 shadowBlur: 10,
