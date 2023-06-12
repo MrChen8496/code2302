e9839637597f32b45e688ac72f146e77bb164851
CL <cl@tedu.cn>
2023-06-12 16:52:24 +0800

    2023年6月12日16:52:21 - 实现影院的列表业务

--- day03/demo/bmdstudios-ms-client/src/views/cinema/CinemaList.vue
+++ day03/demo/bmdstudios-ms-client/src/views/cinema/CinemaList.vue
@@ -1,11 +1,71 @@
 <template>
   <div>
-    <h2>电影院列表</h2>
+    <!-- 地图区域 -->
+    <div 
+      id="container" 
+      style="height:200px; border:1px solid #666;"></div>
+    <el-divider content-position="left">影院列表</el-divider>
+    <!-- 表格数据 -->
+    <el-table :data="cinemas">
+      <el-table-column label="影院名称" width="250px">
+        <template slot-scope="scope">
+          <i class="el-icon-video-camera"></i>
+          {{scope.row.cinema_name}}
+        </template>
+      </el-table-column>
+      <el-table-column label="影院详细地址">
+        <template slot-scope="scope">
+          <i class="el-icon-s-flag"></i>
+          {{scope.row.address}}
+        </template>
+      </el-table-column>
+      <el-table-column label="影院位置" width="230px">
+        <template slot-scope="scope">
+          {{scope.row.province}}
+          {{scope.row.city}}
+          {{scope.row.district}}
+        </template>
+      </el-table-column>
+      <el-table-column label="操作" width="230px">
+        <template>
+          <el-button size="small" type="info" icon="el-icon-position" circle></el-button>
+          <el-button size="small" type="success" icon="el-icon-thumb" circle></el-button>
+          
+          <el-button 
+            size="small" type="warning" icon="el-icon-edit" circle></el-button>
+          <el-button 
+            size="small" type="danger" icon="el-icon-delete" circle></el-button>
+        </template>
+      </el-table-column>
+    </el-table>
   </div>
 </template>
 
 <script>
-export default {};
+import httpApi from '@/http';
+export default {
+
+  data() {
+    return {
+      cinemas: [], // 影院列表
+    }
+  },
+
+  mounted(){
+    this.listCinemas()
+  },
+
+  methods: {
+
+    /** 列出所有影院 */
+    listCinemas() {
+      httpApi.cinemaApi.queryAll().then(res=>{
+        console.log('加载所有影院', res)
+        this.cinemas = res.data.data
+      })
+    }
+  },
+};
 </script>
 
 <style lang="scss" scoped>
