f284c8ca4bc6d675590bb22325dc2bf9556957ae
CL <cl@tedu.cn>
2023-06-12 17:38:39 +0800

    2023年6月12日17:38:37 - 列表页展示地图

--- day03/demo/bmdstudios-ms-client/src/views/cinema/CinemaList.vue
+++ day03/demo/bmdstudios-ms-client/src/views/cinema/CinemaList.vue
@@ -7,7 +7,10 @@
     <el-divider content-position="left">影院列表</el-divider>
     <!-- 表格数据 -->
     <el-table :data="cinemas">
-      <el-table-column label="影院名称" width="250px">
+      <el-table-column 
+        sortable
+        prop="cinema_name"
+        label="影院名称" width="250px">
         <template slot-scope="scope">
           <i class="el-icon-video-camera"></i>
           {{scope.row.cinema_name}}
@@ -43,6 +46,8 @@
 
 <script>
 import httpApi from '@/http';
+import AMapLoader from '@amap/amap-jsapi-loader';
+
 export default {
 
   data() {
@@ -52,10 +57,28 @@ export default {
   },
 
   mounted(){
-    this.listCinemas()
+    this.listCinemas()    
+    // 初始化地图
+    this.initMap();
   },
 
   methods: {
+    /** 初始化地图 */
+    initMap(){
+      AMapLoader.load({
+        key:"7bfbe3ab215345f405c23b5eed760ca8",             // 申请好的Web端开发者Key，首次调用 load 时必填
+        version:"2.0",      // 指定要加载的 JSAPI 的版本，缺省时默认为 1.4.15
+        plugins:['AMap.Geocoder'],       // 需要使用的的插件列表，如比例尺'AMap.Scale'等
+      }).then((AMap)=>{
+        this.map = new AMap.Map("container",{  //设置地图容器id
+          viewMode:"3D",    //是否为3D地图模式
+          zoom:11,           //初始化地图级别
+          center:[116.397509,39.907644], //初始化地图中心点位置
+        });
+      }).catch(e=>{
+        console.log(e);
+      })
+    },
 
     /** 列出所有影院 */
     listCinemas() {
