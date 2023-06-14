f3259dd77fc777615d7e4cc535c3554d5dc997b4
CL <cl@tedu.cn>
2023-06-14 09:36:23 +0800

    2023年6月14日09:36:21 - 显示排片计划表格信息

--- day03/demo/bmdstudios-ms-client/src/views/cinema/ShowingonPlanList.vue
+++ day03/demo/bmdstudios-ms-client/src/views/cinema/ShowingonPlanList.vue
@@ -14,6 +14,22 @@
     </p>
     <el-divider></el-divider>
 
+    <!-- 显示表格数据 -->
+    <el-table :data="plans">
+      <el-table-column label="电影名称" prop="title"></el-table-column>
+      <el-table-column label="放映时间" prop="showingon_date"></el-table-column>
+      <el-table-column label="场次" prop="showingon_time"></el-table-column>
+      <el-table-column label="票价" prop="price"></el-table-column>
+      <el-table-column label="是否已发布">
+        <el-switch></el-switch>
+      </el-table-column>
+      <el-table-column label="操作">
+        <el-button 
+            size="small" type="danger" 
+            icon="el-icon-delete" circle></el-button>
+      </el-table-column>
+    </el-table>
+
   </div>
 </template>
 
@@ -23,16 +39,28 @@ export default {
   mounted(){
     // 显示放映厅的基本信息
     this.showRoomInfo()
+    // 列出当前放映厅已经配置好的计划列表
+    this.listPlans()
   },
   
   data() {
     return {
+      plans: [],    // 存储排片计划列表数据
       movies: [],   // 存储模糊查询的电影列表结果
       roomInfo: {}, // 存储放映厅的基本信息
     }
   },
 
   methods: {
+    /** 列出所有排片计划 */
+    listPlans(){
+      let params = {room_id: this.$route.query.id}
+      httpApi.showingonPlanApi.queryByRoomId(params).then(res=>{
+        console.log('查到的排片计划列表', res)
+        this.plans = res.data.data
+      })
+    },
+
     showRoomInfo() {
       // 从上一页传来的参数中获取放映厅ID
       let id = this.$route.query.id  
