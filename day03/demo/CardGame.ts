ecc2b6be6b59de64f90a937b1fbe41a8054ac509
CL <cl@tedu.cn>
2023-06-19 14:52:11 +0800

    2023年6月19日14:52:08 - 扑克牌（2）

--- day03/demo/CardGame.ts
+++ day03/demo/CardGame.ts
@@ -40,6 +40,77 @@ cards.push(new Card(Suit.王, Rank.Black));
 cards.push(new Card(Suit.王, Rank.Red));
 console.log(cards.toString())
 
+// 声明一个类型，描述玩家Player
+class Player {
+  name: string;
+  score: number;
+  cards: Card[];
+  
+  constructor(name:string){ // 创建Player对象时仅仅提供名字即可
+    this.name = name
+    this.score = 0
+    this.cards = []
+  }
+
+  /** 从总牌堆中随机抽一张，放入自己的手牌中 */
+  choupai(cards:Card[]){
+    // 随机一个下标，从总牌堆中删掉这一张，并且放入手牌
+    let index = Math.floor(Math.random() * cards.length)
+    let deletedCard = cards.splice(index, 1)
+    this.cards.push(deletedCard[0])
+  }
+
+  /** 出牌  将手牌最上方的一张牌打出去 */
+  chupai(){
+    return this.cards.pop()
+  }
+
+  toString(){
+    console.log(`姓名:${this.name}`)
+    console.log(`得分:${this.score}`)
+    console.log(`手牌:${this.cards.toString()}`)
+  }
+
+}
+// 创建两个玩家
+let p1 = new Player('亮亮');
+let p2 = new Player('小新');
+console.log(p1)
+console.log(p2)
+
+// 让两个人交替不断的从总牌堆中随机抽牌，抽到自己手里。最终每人27张
+while(cards.length!=0){
+  p1.choupai(cards)
+  p2.choupai(cards)
+}
+console.log('两人抽牌后--------------------------------')
+p1.toString()
+p2.toString()
+
+// 两人分别出牌，比较大小   计分
+while(true){
+  let c1 = p1.chupai()
+  let c2 = p2.chupai()
+  if(c1!=undefined && c2!=undefined){ // 还能继续比
+    if(c1.rank > c2.rank){
+      p1.score++
+    }else if(c1.rank < c2.rank){
+      p2.score++
+    }
+  }else {
+    break;
+  }
+}
+p1.toString()
+p2.toString()
+
+
+
+
+
+
+
+
 
 
 
