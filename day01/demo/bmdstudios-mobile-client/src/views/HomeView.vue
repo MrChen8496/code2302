d8bac760860dff0eec3658de315fc3cbd96c32e4
CL <cl@tedu.cn>
2023-06-20 17:07:50 +0800

    2023年6月20日17:07:48 - 准备地步选项卡

--- day01/demo/bmdstudios-mobile-client/src/views/HomeView.vue
+++ day01/demo/bmdstudios-mobile-client/src/views/HomeView.vue
@@ -1,18 +1,23 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <div>
+    <p>上半部分</p>
+
+    <!-- 底部选项卡 -->
+    <van-tabbar v-model="active">
+      <van-tabbar-item icon="home-o">标签</van-tabbar-item>
+      <van-tabbar-item icon="search">标签</van-tabbar-item>
+      <van-tabbar-item icon="friends-o">标签</van-tabbar-item>
+      <van-tabbar-item icon="setting-o">标签</van-tabbar-item>
+    </van-tabbar>
   </div>
 </template>
 
-<script lang="ts">
-import { defineComponent } from 'vue';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+<script setup lang="ts">
+import {ref} from 'vue'
+const active = ref(0)
 
-export default defineComponent({
-  name: 'HomeView',
-  components: {
-    HelloWorld,
-  },
-});
 </script>
+
+<style scoped>
+
+</style>
