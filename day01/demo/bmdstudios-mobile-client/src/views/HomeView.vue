66e3ddfcf84f1228b45264892b4b608db53b79ff
CL <cl@tedu.cn>
2023-06-21 10:59:42 +0800

    2023年6月21日10:59:40 - 完成嵌套路由，地步选项卡的点击跳转

--- day01/demo/bmdstudios-mobile-client/src/views/HomeView.vue
+++ day01/demo/bmdstudios-mobile-client/src/views/HomeView.vue
@@ -4,8 +4,11 @@
     <router-view />
 
     <!-- 底部选项卡 -->
-    <van-tabbar v-model="active" active-color="#f03d37">
-      <van-tabbar-item>
+    <van-tabbar 
+      route
+      v-model="active" 
+      active-color="#f03d37">
+      <van-tabbar-item replace to="/home/index">
         首页
         <template #icon="props">
           <img :src="require(`../assets/tabs/index_${
@@ -13,21 +16,21 @@
           }.png`)" alt="">
         </template>
       </van-tabbar-item>
-      <van-tabbar-item>
+      <van-tabbar-item replace to="/home/video">
         视频
         <template #icon="props">
           <img v-show="props.active" src="/tabs/video_1.png" alt="">
           <img v-show="!props.active" src="../assets/tabs/video_0.png" alt="">
         </template>
       </van-tabbar-item>
-      <van-tabbar-item>
+      <van-tabbar-item replace to="/home/show">
         演出
         <template #icon="props">
           <img v-show="props.active" src="../assets/tabs/show_1.png" alt="">
           <img v-show="!props.active" src="../assets/tabs/show_0.png" alt="">
         </template>
       </van-tabbar-item>
-      <van-tabbar-item>
+      <van-tabbar-item replace to="/home/me">
         我的
         <template #icon="props">
           <img v-show="props.active" src="../assets/tabs/me_1.png" alt="">
