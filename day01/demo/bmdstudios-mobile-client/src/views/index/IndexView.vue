dc6088e6a1cd0329ebf31d962fcca873403ddc09
CL <cl@tedu.cn>
2023-06-21 14:13:26 +0800

    2023年6月21日14:13:23 - 完成导航基础样式

--- day01/demo/bmdstudios-mobile-client/src/views/index/IndexView.vue
+++ day01/demo/bmdstudios-mobile-client/src/views/index/IndexView.vue
@@ -14,15 +14,14 @@
     <!-- 导航条 -->
     <div class="nav">
       <span>北京</span>
-      <i></i>
+      <b></b>
       <!-- tabs -->
-      <van-tabs v-model:active="activeName">
+      <van-tabs class="tabs" v-model:active="activeName">
         <van-tab title="热映" name="1"></van-tab>
         <van-tab title="待映" name="2"></van-tab>
         <van-tab title="经典" name="3"></van-tab>
       </van-tabs>
       <van-icon name="search" />
-
     </div>
   </div>
 </template>
@@ -32,6 +31,31 @@ import { ref } from 'vue'
 const activeName = ref('1')
 </script>
 
-<style scoped>
+<style scoped lang="scss">
+.nav {
+  display: flex;
+  align-items: center;
+  padding: 0px 15px;
+  border-bottom: 1px solid #eee;
+
+  b {
+    display: block;
+    border: 4px solid transparent;
+    border-top-color: black;
+    margin: 8px 0px 0px 4px;
+  }
+
+  .tabs {
+    flex: 1;
+    margin: 0px 20px;
+  }
+
+  i {
+    font-weight: bold;
+    font-size: 24px;
+    color: #f03d37;
+  }
+  
+}
 
 </style>
